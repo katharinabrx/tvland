6992446b84c8154ead24dc8f79212edffafcfc49
Katharina <mail@katharina.dev>
2020-01-21 00:06:14 +0100

    Updates

---
+++ precache-manifest.13276222e912675b6c3243d0dd9a7d05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502c7a13cfb3b66a7145666dcaaca100",
+    "url": "/tvland/index.html"
+  },
+  {
+    "revision": "600fa87afb78cf409fd4",
+    "url": "/tvland/static/css/main.32b24f41.chunk.css"
+  },
+  {
+    "revision": "d43abc5fca3725908766",
+    "url": "/tvland/static/js/2.6f035f7a.chunk.js"
+  },
+  {
+    "revision": "f8d07a0791b71cec298a1ad1be623170",
+    "url": "/tvland/static/js/2.6f035f7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "600fa87afb78cf409fd4",
+    "url": "/tvland/static/js/main.8113313a.chunk.js"
+  },
+  {
+    "revision": "a2821cbe2b4b718c82f0",
+    "url": "/tvland/static/js/runtime-main.705e775f.js"
+  },
+  {
+    "revision": "d6cf0c0eee905601f24a235e80a33b20",
+    "url": "/tvland/static/media/arrow-back.d6cf0c0e.svg"
+  },
+  {
+    "revision": "75e2a7cda753a641d273d7581511abf8",
+    "url": "/tvland/static/media/placeholder.75e2a7cd.jpg"
+  }
+]);
